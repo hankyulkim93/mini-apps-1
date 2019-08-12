f82ad8ba94375fb9dcf39ae8bf6fa738fb93fe3f
Han Kim <bagsangcheol@HanKyulKim-MacBook-Air.local>
2019-08-12 13:58:49 -0700

    added table, begins with X, alternates between X & O, New Game button restarts game

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,38 @@
+//event listener / clicker
+//clicker does not happen when table border is clicked
 
+let player = 1;
+let cells = document.querySelectorAll("td");
 
+cells.forEach(function(cell) {
+    cell.addEventListener('click', function() {
+        if (!cell.innerHTML) {
+            if (player % 2 !== 0) {
+                cell.innerHTML = 'X';
+                player++;
+            } else {
+                cell.innerHTML = 'O';
+                player++;
+            }
+        }
+    })
+})
+
+//restart game w/ button
+
+let restartButton = document.getElementById('restart');
+
+restartButton.addEventListener('click', function() {
+    cells.forEach(function(cell) {
+        cell.innerHTML = '';
+        player = 1;
+    });
+});
+
+//display message when board is full
+
+// message() = {
+//     if (player === 9) {
+//         alert('STOP');
+//     }
+// }
