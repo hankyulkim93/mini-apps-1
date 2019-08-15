ac61deeded3d76f2667d9771577a582de9a3f56b
Han Kim <bagsangcheol@HanKyulKim-MacBook-Air.local>
2019-08-15 12:24:24 -0700

    finished pages & buttons for transitions, need to work on app.post & database implementation

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -6,16 +6,33 @@ class App extends React.Component {
             page: 0,
             name: '',
             email: '',
-            password: ''
+            password: '',
+            addressLineOne: '',
+            addressLineTwo: '',
+            city: '',
+            state: '',
+            zipCode: '',
+            phoneNumber: '',
+            creditCard: '',
+            expiryDate: '',
+            cvv: '',
+            billZipCode: ''
         }
 
         this.handleNext = this.handleNext.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handlePurchase = this.handlePurchase.bind(this);
     }
  
     handleNext(e) {
         this.setState(
             {page: this.state.page + 1}
+        )
+    }
+
+    handlePurchase(e) {
+        this.setState(
+            {page: 0}
         )
     }
 
@@ -46,13 +63,87 @@ class App extends React.Component {
                     <input type='text' id='password' onChange={this.handleChange} value={this.state.password}></input>
                 </label><br></br>
             </form>
+            <button onClick={this.handleNext} id='nextPage'>Next</button>
             </div>
+            )
+        } else if (this.state.page === 2) {
+            return (
+            <div>
+            <form id='f2'>
+                <label>
+                    Address Line 1:
+                    <input type='text' id='addressLineOne' onChange={this.handleChange} value={this.state.addressLineOne}></input>
+                </label><br></br>
+                <label>
+                    Address Line 2:
+                    <input type='text' id='addressLineTwo' onChange={this.handleChange} value={this.state.addressLineTwo}></input>
+                </label><br></br>
+                <label>
+                    City:
+                    <input type='text' id='city' onChange={this.handleChange} value={this.state.city}></input>
+                </label><br></br>
+                <label>
+                    State:
+                    <input type='text' id='state' onChange={this.handleChange} value={this.state.state}></input>
+                </label><br></br>
+                <label>
+                    Zip Code:
+                    <input type='text' id='zipCode' onChange={this.handleChange} value={this.state.zipCode}></input>
+                </label><br></br>
+                <label>
+                    Phone Number:
+                    <input type='text' id='phoneNumber' onChange={this.handleChange} value={this.state.phoneNumber}></input>
+                </label><br></br>
+            </form>
+            <button onClick={this.handleNext} id='nextPage'>Next</button>
+            </div>
+            )
+        } else if (this.state.page === 3) {
+            return (
+                <div>
+                <form id='f3'>
+                    <label>
+                        Credit Card #:
+                        <input type='text' id='creditCard' onChange={this.handleChange} value={this.state.creditCard}></input>
+                    </label><br></br>
+                    <label>
+                        Expiry Date:
+                        <input type='text' id='expiryDate' onChange={this.handleChange} value={this.state.expiryDate}></input>
+                    </label><br></br>
+                    <label>
+                        CVV:
+                        <input type='text' id='cvv' onChange={this.handleChange} value={this.state.cvv}></input>
+                    </label><br></br>
+                    <label>
+                        Billing Zip Code:
+                        <input type='text' id='billZipCode' onChange={this.handleChange} value={this.state.billZipCode}></input>
+                    </label><br></br>
+                </form>
+                <button onClick={this.handleNext} id='nextPage'>Next</button>
+                </div>
+                )
+        } else if (this.state.page === 4) {
+            return (
+                <div id='final'>
+                    <h2>Confirmation</h2>
+                    <div>Name: {this.state.name}</div>
+                    <div>Email: {this.state.email}</div>
+                    <div>Password: {this.state.password}</div>
+                    <div>Address Line 1: {this.state.addressLineOne}</div>
+                    <div>Address Line 2: {this.state.addressLineTwo}</div>
+                    <div>City: {this.state.city}</div>
+                    <div>State: {this.state.state}</div>
+                    <div>Zip Code: {this.state.zipCode}</div>
+                    <div>Phone Number: {this.state.phoneNumber}</div>
+                    <div>Credit Card #: {this.state.creditCard}</div>
+                    <div>Expiry Date: {this.state.expiryDate}</div>
+                    <div>CVV: {this.state.cvv}</div>
+                    <div>Billing Zip Code: {this.state.billZipCode}</div>
+                <button onClick={this.handlePurchase} id='purchase'>Purchase</button>
+                </div>
             )
         }
     }
-
-
-
 }
 // page 0 = checkout button
 // page 1 = F1 + next button (name, email, password)
