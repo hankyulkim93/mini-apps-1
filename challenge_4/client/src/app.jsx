a82aa72328c735ec07734b021025af30ab9f4bc5
Han Kim <bagsangcheol@HanKyulKim-MacBook-Air.local>
2019-08-15 17:34:15 -0700

    connected to server, basic rendering

--- challenge_4/client/src/app.jsx
+++ challenge_4/client/src/app.jsx
@@ -1,1 +1,12 @@
-console.log('hi')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return(
+        <div>
+            test
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.post('/', (req, res) => {
+    res.send('CONNECTED');
+});
+
+// app.get
+
+app.listen(port, () => {console.log(`Connected to port: ${port}`)});
 
 
